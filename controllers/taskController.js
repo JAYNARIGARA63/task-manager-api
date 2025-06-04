8b5e64a4852422a58ef23f043ce8e7f3e8e6a47e
jay <jaynarigara4@gmail.com>
2025-06-05 00:07:43 +0530

    create add task api

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -4,3 +4,10 @@ exports.getAllTasks = async (req, res) => {
   const tasks = await Task.getAllTasks();
   res.json(tasks);
 };
+
+exports.createTask = async (req, res) => {
+  const taskBody = req.body;
+
+  const newTask = await Task.createTask(taskBody);
+  res.status(201).json(newTask);
+};
--- models/taskModel.js
+++ models/taskModel.js
@@ -4,3 +4,12 @@ exports.getAllTasks = async () => {
   const result = await db.query("SELECT * FROM tasks ORDER BY id DESC");
   return result.rows;
 };
+
+exports.createTask = async (task) => {
+  const { user_id, title, description } = task;
+  const result = await db.query(
+    "INSERT INTO tasks (user_id,title,description) VALUES ($1,$2,$3) RETURNING *",
+    [user_id, title, description]
+  );
+  return result.rows[0];
+};
